86f1a63dc0ed72744ca3f603a96cbc018f690d56
Filip Angelov <filyox3@gmail.com>
2017-07-06 12:16:26 +0300

    Ready for Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -3,7 +3,8 @@ use Mix.Config
 
 config :cmsx3, Cmsx3.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "cmsx3-api.herokuapp.com", port: 443],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
